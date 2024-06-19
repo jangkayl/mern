752304751bc96f2ef6c6412d1b67caa217270f6c
JohnKyleV <villarazojk@gmail.com>
2024-06-19 14:45:59 +0800

    as

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
 	cors({
-		origin: ["/*", "*"],
+		origin: ["https://kylemern.vercel.app"],
 		methods: ["GET", "POST", "PUT", "DELETE"],
 		credentials: true,
 	})
