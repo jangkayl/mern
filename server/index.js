d51002c430cd125efe6843acd0ff3ecc1558cd34
JohnKyleV <villarazojk@gmail.com>
2024-06-19 13:45:48 +0800

    asd

--- server/index.js
+++ server/index.js
@@ -20,6 +20,15 @@ app.use(
 	})
 );
 
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "https://kylemern.vercel.app");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
+
 mongoose
 	.connect(mongoDBURL)
 	.then(() => {
